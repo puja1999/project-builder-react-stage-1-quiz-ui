368cf126971863afbec0176e6d2e55eab41ef0af
Puja <pujasah2501@gmai.com>
2021-05-23 00:31:40 +0530

    Completed

---
+++ my-app/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../styles/home.css';
+
+ class Home extends Component{
+    render(){
+        return(
+            <div className="home-parent">
+            <div className="home-cont">
+                <h1>Quiz App</h1>
+                <button>Play</button>
+            </div>
+            </div>
+        )
+    }
+}
+export default Home;
+
+ 
